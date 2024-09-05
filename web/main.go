6c5e1be10bd2f6ee865790690c5cec0cf468ad2d
Pedro Henrique de Oliveira Monteiro <ph817285@gmail.com>
2024-09-05 15:09:42 -0300

    Requisição com Ajax

--- web/main.go
+++ web/main.go
@@ -1,16 +1,31 @@
 package main // Nome do Arquivo
 
 import (
-	"fmt" //Importardo o formatador
-	"net/http" // Importando a biblioteca requisições http entre outros, 
+	"encoding/json"
+	"fmt"      //Importardo o formatador
+	"net/http" // Importando a biblioteca requisições http entre outros,
 )
 
+func GetData(w http.ResponseWriter, r *http.Request)  {
+
+	 data := "Olá Mundo!!!!"
+
+	 w.Header().Set("Content-Type", "application/json")
+
+	 err := json.NewEncoder(w).Encode(data)
+
+	 if err != nil {
+		http.Error(w, "Falha ao codificar resposta", http.StatusInternalServerError)
+		
+	 }
+}
 func main() { // Função de entrada
 
+ 
 	fs := http.FileServer(http.Dir("static")) // Definindo o diretório na pasta 'static'
 
 	http.Handle("/", fs) //Mapeando a raiz do servidor para o FIleServer
-
+	http.HandleFunc("/data", GetData)
 	fmt.Println("Servidor rodando na porta 8080...") // Mensagem informando a porta
 
 	err := http.ListenAndServe(":8080", nil) //Porta principal 8080
--- web/static/script.js
+++ web/static/script.js
@@ -1,3 +1,17 @@
-document.getElementById("teste").addEventListener("click", () => {
-    alert("Você clicou!!!!!")
+document.getElementById("teste").addEventListener('click', function() {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', 'http://localhost:8080/data')
+    xhr.onload = function() {
+        if(xhr.status ===  200) {
+            document.getElementById("result").innerText = xhr.responseText
+        } else {
+            document.getElementById("result").innerText = 'Error' + xhr.status
+        }
+    }
+    xhr.onerror = function() {
+        document.getElementById("result").innerHTML = "Erro na requisição"
+        
+    }
+    xhr.send()
 })
+
