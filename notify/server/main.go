dfe2bfbb85bb129c918131857bdcb6bc98bcc900
Pedro Henrique de Oliveira Monteiro <ph817285@gmail.com>
2024-11-19 08:45:34 -0300

    Notificação em tempo real
    Inicio de um projeto de notificação usando websocket para uma conexão de cliente e servidor.

--- notify/server/main.go
+++ notify/server/main.go
@@ -3,14 +3,20 @@ package main
 import (
     "log"
     "net/http"
+    "sync"
+
     "github.com/gorilla/websocket"
 )
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan []byte)
+var mutex = sync.Mutex{}
 
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
     WriteBufferSize: 1024,
     CheckOrigin: func(r *http.Request) bool {
-        return true // Permitir conexões de qualquer origem
+        return true
     },
 }
 
@@ -22,26 +28,54 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
     }
     defer ws.Close()
 
-    log.Println("Conexão WebSocket estabelecida com sucesso!")
+    mutex.Lock()
+    clients[ws] = true
+    mutex.Unlock()
+
+    log.Println("Novo cliente conectado.")
 
     for {
         _, msg, err := ws.ReadMessage()
         if err != nil {
             log.Printf("Erro ao ler mensagem: %v", err)
+            mutex.Lock()
+            delete(clients, ws)
+            mutex.Unlock()
             break
         }
         log.Printf("Mensagem recebida: %s", msg)
-        // Envia a mensagem de volta para todos os clientes conectados
-        err = ws.WriteMessage(websocket.TextMessage, msg)
-        if err != nil {
-            log.Printf("Erro ao enviar mensagem: %v", err)
-            break
+        broadcast <- msg
+    }
+}
+
+
+func handleMessages() {
+    for {
+
+        msg := <-broadcast
+        mutex.Lock()
+        for client := range clients {
+            err := client.WriteMessage(websocket.TextMessage, msg)
+            if err != nil {
+                log.Printf("Erro ao enviar mensagem: %v", err)
+                client.Close()
+                delete(clients, client)
+            }
         }
+        mutex.Unlock()
     }
 }
 
 func main() {
     http.HandleFunc("/ws", handleConnections)
+
+    fs := http.FileServer(http.Dir("../client"))
+
+	http.Handle("/", fs)
+
+    go handleMessages()
+
+
     log.Println("Servidor iniciado na porta 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
